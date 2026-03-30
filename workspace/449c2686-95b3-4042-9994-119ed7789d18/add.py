6f9c5ebfe916cf8a8ece729b44ec956b6cae61f1
Shivam Mishra <131511555+ShivamCore@users.noreply.github.com>
2026-03-31 05:16:39 +0530

    Delete workspace directory

--- workspace/449c2686-95b3-4042-9994-119ed7789d18/add.py
+++
@@ -1,7 +0,0 @@
-
-
-# Main script to call the add() function
-if __name__ == '__main__':
-    import add
-    result = add.add()
-    print(result)
--- workspace/aca4e5b3-5240-446e-8e88-e646f0e2fd9b/add.py
+++
@@ -1,6 +0,0 @@
-
-
-# Main script to call the add() function
-if __name__ == '__main__':
-    result = add()
-    print(result)
